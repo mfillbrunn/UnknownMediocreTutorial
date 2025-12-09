8693b4e7fea4d94fba918e1d39b3cdac9f2f89a0
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2025-12-09 22:14:06 +0000

    Update emitState.js

--- server/utils/emitState.js
+++ server/utils/emitState.js
@@ -2,10 +2,11 @@
 const { buildSafeStateForPlayer } = require("./safeState");
 
 function emitStateForAllPlayers(roomId, room, io) {
-  console.log("EMITTING SAFE STATE FOR PLAYER", playerId, JSON.stringify(safe, null, 2));
+
   for (const [playerId, role] of Object.entries(room.players)) {
     const safe = buildSafeStateForPlayer(room.state, role);
     io.to(playerId).emit("stateUpdate", safe);
+      console.log("EMITTING SAFE STATE FOR PLAYER", playerId, JSON.stringify(safe, null, 2));
   }
 }
 
