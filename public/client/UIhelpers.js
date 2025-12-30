eca7c327661a92a338c3db887842dc7d85bed082
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2025-12-30 10:52:28 +0100

    Update UIhelpers.js

--- public/client/UIhelpers.js
+++ public/client/UIhelpers.js
@@ -207,25 +207,4 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-///ROOM ID DISPLAY
-(() => {
-  const syncHeaderRoomCodes = () => {
-    const src = document.getElementById("roomCodeLabel");
-    if (!src) return;
-
-    document
-      .querySelectorAll(".header-room-code")
-      .forEach(el => {
-        el.textContent = src.textContent;
-      });
-  };
-
-  const src = document.getElementById("roomCodeLabel");
-  if (!src) return;
-
-  const observer = new MutationObserver(syncHeaderRoomCodes);
-  observer.observe(src, { childList: true });
-
-  syncHeaderRoomCodes();
-})();
 
