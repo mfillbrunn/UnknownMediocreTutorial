00fa46ea132b2863b9a3212bc758812589fc79b8
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-01 21:55:45 +0000

    Update simultaneous.js

--- server/core/phases/simultaneous.js
+++ server/core/phases/simultaneous.js
@@ -9,11 +9,12 @@ function handleSimultaneousPhase(room, state, action, role, roomId, context) {
   const { ALLOWED_GUESSES, powerEngine } = context;
   const { isValidWord } = require("../../game-engine/validation");
 
-if (state.timeControl.enabled ) {
+if (state.timeControl.enabled && state.isTimerRunning === false ) {
     state.activeTimer = "both";
     resetRoundTimer(state);
     state.roundStartTime = Date.now();
     startGameTimer(room, state, roomId, context);
+    state.isTimerRunning = true;
   }
   // ---------------------------------------------
   // SETTER submits initial secret
