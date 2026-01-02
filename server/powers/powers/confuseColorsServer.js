a77a17338d4d378bf629eed08cfd02966b5210b5
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-02 18:37:55 +0000

    Update confuseColorsServer.js

--- server/powers/powers/confuseColorsServer.js
+++ server/powers/powers/confuseColorsServer.js
@@ -8,7 +8,6 @@ engine.registerPower("confuseColors", {
     if (state.powers.confuseColorsUsed) return;
 
     state.powers.confuseColorsUsed = true;
-    state.powers.confuseColorsActive = true;
 
     io.to(roomId).emit("powerUsed", { type: "confuseColors" });
   },
@@ -27,7 +26,7 @@ engine.registerPower("confuseColors", {
     });
     entry.powerUsed = "ConfuseColors";
   }
-  state.powers.confuseColorsActive = false;
+  state.powers.confuseColorsActive = true;
 }
 
 
