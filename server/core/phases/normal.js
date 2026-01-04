7d86d844160c96f88f09517ed06ca9f1e5a8a5e1
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-04 15:53:26 +0000

    Update normal.js

--- server/core/phases/normal.js
+++ server/core/phases/normal.js
@@ -13,6 +13,7 @@ function handleNormalPhase(room, state, action, role, roomId, context) {
   /// GUESSER SUBMIT
   if (!state.pendingGuess && action.type === "SUBMIT_GUESS" && role === state.guesser) {
     const g = action.guess.toLowerCase(); 
+    state.guessCount=+1;
     state.timeUsed[state.guesser] +=  Math.floor((Date.now() - state.roundStartTime) / 1000);
     state.roundStartTime = Date.now();
     // ASSASSIN
