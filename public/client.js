ef163dc7650a877d0e399f03fe0a6d77fdc890a3
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-07 19:38:33 +0000

    Update client.js

--- public/client.js
+++ public/client.js
@@ -12,7 +12,7 @@ let KeepEnabled = true;
 let NewEnabled = true;
 window.state = null;
 const VOWELS = new Set(["A", "E", "I", "O", "U"]);
-import { supabase } from "./supabaseClient.js";
+import { supabase } from "../../authentication/supabaseClient.js";
 // -----------------------------------------------------
 // DOM HELPERS
 // -----------------------------------------------------
