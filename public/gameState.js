1cc22e660f9ce53660a0cca32a554061ded4e06c
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-07 19:53:31 +0000

    Create gameState.js

---
+++ public/gameState.js
@@ -0,0 +1,12 @@
+// gameState.js
+export let roomId = null;
+export let state = null;
+export let myRole = null;
+
+export function setRoomId(id) {
+  roomId = id;
+}
+
+export function setState(newState) {
+  state = newState;
+}
