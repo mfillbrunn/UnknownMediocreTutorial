fdd9c455ae16d8effff39fccc6abb761b77093c8
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-07 19:31:33 +0000

    Create supabaseClient.js

---
+++ public/authentication/supabaseClient.js
@@ -0,0 +1,7 @@
+// supabaseClient.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://zskbwatoxvghyouqjuxi.supabase.co",
+  "sb_publishable_1ubwHN-WJJcvYRQb9c5Y9g_1FdOPGAP"
+);
