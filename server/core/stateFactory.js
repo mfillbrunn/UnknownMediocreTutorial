913fcb6ad0c165883e74fac53762690d9243b9e6
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-09 20:40:41 +0000

    Update stateFactory.js

--- server/core/stateFactory.js
+++ server/core/stateFactory.js
@@ -11,6 +11,7 @@ function createInitialState() {
 
   // --- MODE / MATCH CONTROL (NEW, GENERIC) ---
     mode: null,          // instance of mode controller
+    rankMode: "bullet",
     matchMeta: {},       // owned entirely by the mode
     ranked : false,
     gameOverView: "match",
