0ee71bbaa1f3d85d1ed91bee7a9d9ef27981d1aa
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-09 19:11:41 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const registerSocketHandlers = require("./network/socketHandlers");
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY // IMPORTANT: service role
 );
 // ------------------------------
