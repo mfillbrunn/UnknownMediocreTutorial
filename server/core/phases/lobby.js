8f9f9302dc2a477e98492b3991b1138f805fe81b
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-10 23:20:33 +0000

    Update lobby.js

--- server/core/phases/lobby.js
+++ server/core/phases/lobby.js
@@ -41,12 +41,12 @@ if (action.type === "PLAYER_JOINED") {
   return;
 }
 if (action.type === "SET_RANKED") {
-  if (room.state.host !== action.playerId) return;
+  if (state.hostUserId !== action.playerId) return;
   state.ranked = !!action.ranked;
 }
 
 if (action.type === "SET_TIME_CONTROL") {
-  if (state.host !== action.playerId) return state;
+  if (state.hostUserId !== action.playerId) return state;
 
  if (action.enabled === false) {
     state.timeControl.enabled = false;
