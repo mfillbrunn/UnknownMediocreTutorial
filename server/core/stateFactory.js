4e90e573efcad90b2fdff2306d3ea6090340c164
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-12 22:52:51 +0000

    Update stateFactory.js

--- server/core/stateFactory.js
+++ server/core/stateFactory.js
@@ -8,8 +8,6 @@ function createInitialState() {
     phase: "lobby",
     host:null,
     roles:{},
-    matchWinner:"",
-    matchWinReason:"",
   // --- MODE / MATCH CONTROL (NEW, GENERIC) ---
     mode: null,          // instance of mode controller
     rankMode: "bullet",
