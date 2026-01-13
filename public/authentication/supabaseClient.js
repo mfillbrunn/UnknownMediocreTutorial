ae71b2c4c434b9d93563f62b11b1afc6eb702261
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-13 21:10:14 +0000

    Update supabaseClient.js

--- public/authentication/supabaseClient.js
+++ public/authentication/supabaseClient.js
@@ -1,7 +1,6 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-  window.supabase = supabase.createClient(
-    "https://zskbwatoxvghyouqjuxi.supabase.co",
-    "sb_publishable_1ubwHN-WJJcvYRQb9c5Y9g_1FdOPGAP"
-  );
-</script>
+// client/supabase.js  (PURE JS FILE)
+
+window.supabase = supabase.createClient(
+  "https://zskbwatoxvghyouqjuxi.supabase.co",
+  "sb_publishable_1ubwHN-WJJcvYRQb9c5Y9g_1FdOPGAP"
+);
