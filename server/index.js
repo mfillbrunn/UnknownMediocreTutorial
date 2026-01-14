8602b555ce3425defd8ef85928aba38721a32a7b
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-14 19:18:59 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ require("./powers/powers/blindSpotServer");
 require("./powers/powers/stealthGuessServer");
 require("./powers/powers/magicModeServer.js");
 require("./powers/powers/vowelRefreshServer.js");
+require("./powers/powers/nonsenseServer.js");
 require("./powers/powers/revealLetterServer.js");
 require("./powers/powers/assassinWordServer.js");
 
