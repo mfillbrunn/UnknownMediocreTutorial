09d91cdaa36e09ba4b4fcdfa94659ab803caecd0
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-17 19:22:35 +0000

    Update resetRoundState.js

--- server/utils/resetRoundState.js
+++ server/utils/resetRoundState.js
@@ -1,5 +1,5 @@
 const {createInitialPowers} = require("../core/stateFactory");
-const {resetRoundTimer} = require("./chessTimer");
+const {resetRoundTimer, startTimer} = require("./chessTimer");
 function resetRoundState(room, state, roomId, context) {
   state.secret = "";
   state.currentSecret = "";
