ba6a7c1b63b0d9b27d0f68e221b9721a11bf563f
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-17 19:33:46 +0000

    Update stateMachine.js

--- server/core/stateMachine.js
+++ server/core/stateMachine.js
@@ -4,9 +4,11 @@ const handleLobbyPhase = require("./phases/lobby");
 const handleSimultaneousPhase = require("./phases/simultaneous");
 const { handleNormalPhase } = require("./phases/normal");
 const {handleGameOverPhase} = require("./phases/gameOver");
+const {maybeRunAI} = require("./ai/runAI");
 
 function applyAction(room, state, action, role, roomId, context) {
   const { powerEngine } = context;
+  maybeRunAI();
   switch (state.phase) {
     case "lobby":
       return handleLobbyPhase(room, state, action, role, roomId, context);
