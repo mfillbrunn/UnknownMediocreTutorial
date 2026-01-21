6c87a67ef08bc1c0163de100d7f2ca1064848086
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-21 22:46:08 +0000

    Update postGame.js

--- server/core/phases/postGame.js
+++ server/core/phases/postGame.js
@@ -2,8 +2,6 @@
 
 const { emitStateForAllPlayers } = require("../../utils/emitState");
 const { emitLobbyEvent } = require("../../utils/emitLobby");
-const { createInitialState } = require("../stateFactory");
-const resetRoundState = require("../../utils/resetRoundState");
 const {startGameTimer} = require("./normal");
 
 
@@ -11,20 +9,10 @@ function handleGameOverPhase(room, state, action, role, roomId, context) {
     const io = context.io;
      ///NEXT ROUND
     if (action.type === "NEXT_ROUND") {
-      if (!state.canNextRound || state.gameOverView !== "round") {return;}
+      if (!state.canNextRound) {return;}
        const res = state.mode?.onNextRound?.(state) || {phase: "simultaneous",resetRound: true};
-        //powers not to reset across rounds
-       let saved;
-       if (state.activePowers.includes("revealLetter")) {
-            saved = state.powers.revealLetter.mode;
-        }    
-       resetRoundState(room, state, roomId, context);
-       if (state.activePowers.includes("revealLetter")) {
-            state.powers.revealLetter.mode = saved;
-        }    
        state.phase = res.phase || "simultaneous";  
        state.gameOver = false;
-       state.gameOverView = "match";
        state.canNextRound = false;   
        if (state.timeControl?.enabled){
            state.paused = false;
@@ -38,25 +26,9 @@ function handleGameOverPhase(room, state, action, role, roomId, context) {
     }  
       ///NEW MATCH
     if (action.type === "NEW_MATCH") {
-        console.log("I'm being pressed");
-      const names = state.playerNames;
-      const fresh = createInitialState();
-       for (const key of Object.keys(state)) {
-         delete state[key];
-       }
-      Object.assign(state, fresh);
       state.phase = "lobby";
       state.ready = {};
-      state.playerNames= names;
-      // Re-sync roles with room.players
-      for (const [playerId, player] of Object.entries(room.players)) {
-        state.roles[playerId] = player.role;
-      }
-      state.setter = "A";
-      state.guesser = "B";
-        console.log("Go to lobby");
       emitLobbyEvent(io, roomId, { type: "enterLobby" });  
-        console.log("didn't go to lobby?");
       emitStateForAllPlayers(roomId, room, io);
       return;
     }
