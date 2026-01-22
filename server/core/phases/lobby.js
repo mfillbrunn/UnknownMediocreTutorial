982b1b35bd567f8b8a4b087fbfd1abdeb4f67575
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-22 23:02:34 +0000

    Update lobby.js

--- server/core/phases/lobby.js
+++ server/core/phases/lobby.js
@@ -150,7 +150,9 @@ if (action.type === "SET_POWER_COUNT") {
           const players = Object.entries(room.players);
           const humanPlayers = players.filter(([_, p]) => !p.isAI);
           const aiPlayers = players.filter(([_, p]) => p.isAI);
-         if (humanPlayers.length ===1 && aiPlayers.length ==0){return;}                
+                console.log("it works");
+         if (humanPlayers.length + aiPlayers.length < 2){return;}  
+                console.log("it did");
           state.ready[action.playerId] = true;               
           emitToOtherPlayer(io, room, action.playerId, {
             type: "playerReady",
