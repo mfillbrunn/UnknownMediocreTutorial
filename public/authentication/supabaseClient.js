3673a1a7b6af01f34ad1f358f6380ef1e0b915dd
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-22 23:42:11 +0000

    Update supabaseClient.js

--- public/authentication/supabaseClient.js
+++ public/authentication/supabaseClient.js
@@ -1,19 +1,17 @@
 // authentication/supabaseClient.js
 
-if (!window.__supabaseInit) {
-  window.__supabaseInit = true;
+if (!window.supabase) {
+  console.log("🔥 Creating Supabase client");
 
   window.supabase = supabase.createClient(
     "https://zskbwatoxvghyouqjuxi.supabase.co",
     "sb_publishable_1ubwHN-WJJcvYRQb9c5Y9g_1FdOPGAP",
     {
       auth: {
-        autoRefreshToken: true,
         persistSession: true,
+        autoRefreshToken: true,
         detectSessionInUrl: false
       }
     }
   );
-
-  console.log("🔥 Supabase client created");
 }
