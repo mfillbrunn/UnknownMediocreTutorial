ed027f94f4d2dbe914f0974b9c4c94ddf66ca2d5
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-25 20:40:44 +0000

    Update client.js

--- public/client.js
+++ public/client.js
@@ -368,8 +368,8 @@ function updateSetterScreen() {
   const setterName = getPlayerNameByCurrentRole(state.setter);
   KeepEnabled=true;
   NewEnabled=true;  
-  $("setterScreen").querySelector(".screen-title").textContent = setterName;
-  //$("setterRoleBadge").textContent = "Spy";
+  $("setterScreen")?.querySelector(".screen-title")?.textContent = setterName;
+  $("setterRoleBadge")?.textContent = "Spy";
   const displayGuess =state.powers?.stealthGuessActive? "?????": state.pendingGuess;
   const isSetterTurn = state.turn === state.setter;
   const isDecisionStep =isSetterTurn &&!!displayGuess &&state.phase === "normal";
@@ -600,8 +600,8 @@ renderDraftRows({
 });
  const guesserName = getPlayerNameByCurrentRole(state.guesser);
   
-  $("guesserScreen").querySelector(".screen-title").textContent = guesserName;
-//$("guesserRoleBadge").textContent = "Inspector";
+  $("guesserScreen")?.querySelector(".screen-title")?.textContent = guesserName;
+  $("guesserRoleBadge")?.textContent = "Inspector";
 setTurn("guesserScreen", false);
 if (state.phase === "simultaneous") {setTurn("guesserScreen", !state.pendingGuess);}
 if (state.phase === "normal" && state.turn === state.guesser) {setTurn("guesserScreen", true);} 
