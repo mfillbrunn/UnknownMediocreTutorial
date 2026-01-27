1c83178a2eae80bfb3dd52d387d737c17d9d9b83
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-27 23:51:47 +0000

    Create aiActions.js

---
+++ server/core/ai/aiActions.js
@@ -0,0 +1,12 @@
+const { emitStateForAllPlayers } = require("../../utils/emitState");
+const {applyAction} = require("../stateMachine");
+
+function applyAIAction(room, action, role, roomId, context) {
+  action.ai = true;
+  action.role = role;
+
+  applyAction(room, room.state, action, role, roomId, context);
+  emitStateForAllPlayers(roomId, room, context.io);
+}
+
+export.modules = {applyAIAction}
