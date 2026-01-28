ef96a6a91191463e98fa4a3f7fa022223e2ee2e5
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-28 21:10:30 +0000

    Update client.js

--- public/client.js
+++ public/client.js
@@ -512,7 +512,7 @@ function applyPreviewFeedback(fbArray) {
 }
 ///SETTER INPUT
 function handleSetterInput(event) {
-  if (!state.powers?.freezeActive || !state.powers?.rouletteSecretActive){
+  if (!state.powers?.freezeActive || !state.powers?.rouletteSecretActive){return;}
     const isNormalSetterTurn =  myRole === state.setter && state.phase === "normal" && state.turn === state.setter && !!state.pendingGuess;
     const isSimultaneousSecretEntry = state.phase === "simultaneous" && !state.secret && !state.simultaneousSecretSubmitted;
     if (!(isNormalSetterTurn || isSimultaneousSecretEntry)) return;
@@ -537,7 +537,6 @@ function handleSetterInput(event) {
       }
       return;
     }
-  }
   if (event.type === "ENTER") {
     const draft = (state.setterDraft || "").trim();
     if (draft.length === 0) {
