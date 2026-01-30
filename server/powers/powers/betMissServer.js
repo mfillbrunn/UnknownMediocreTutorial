78a4df018d55939f7155e2210bb1389dbb39b91a
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-01-30 22:36:22 +0000

    Update betMissServer.js

--- server/powers/powers/betMissServer.js
+++ server/powers/powers/betMissServer.js
@@ -1,1 +1,10 @@
-s
+const engine = require("../powerEngineServer");
+
+engine.registerPower("betMiss", {
+  apply(state, action, roomId, io) {
+    if (state.powers.betMissUsed) return;
+    state.powers.betMissActive = true;
+    state.powers.betMissUsed = true;
+    io.to(roomId).emit("powerUsed", { type: "betMiss" });
+  }
+});
