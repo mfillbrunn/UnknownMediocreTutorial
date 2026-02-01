8bb8a0464be44f3cdf613798384d29cf12123983
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-02-01 22:27:32 +0000

    Update stateMachine.js

--- server/core/stateMachine.js
+++ server/core/stateMachine.js
@@ -1,34 +1,3 @@
-// core/stateMachine.js
+const { applyAction } = require("./state/applyAction");
 
-const handleLobbyPhase = require("./phases/lobby");
-const handleSimultaneousPhase = require("./phases/simultaneous");
-const { handleNormalPhase } = require("./phases/normal");
-const {handleGameOverPhase} = require("./phases/postGame");
-
-
-function applyAction(room, state, action, role, roomId, context) {
-  switch (state.phase) {
-    case "lobby":
-      handleLobbyPhase(room, state, action, role, roomId, context);
-      break;
-
-    case "simultaneous":
-      handleSimultaneousPhase(room, state, action, role, roomId, context);
-      break;
-
-    case "normal":
-      handleNormalPhase(room, state, action, role, roomId, context);
-      break;
-
-    case "gameOver":
-      handleGameOverPhase(room, state, action, role, roomId, context);
-      break;
-
-    default:
-      console.warn("Unknown phase:", state.phase);
-      return;
-  }
-}
-
-
-module.exports = {applyAction};
+module.exports = { applyAction };
