32dbd64bf196fe3c46c25516972a7919e9e65220
mfillbrunn <mkfillbrunn@gmail.com>
2026-02-02 21:33:28 +0000

    s

--- server/core/phases/normal.js
+++ server/core/phases/normal.js
@@ -57,11 +57,6 @@ if (state.pendingGuess && state.turn === state.setter && (action.type === "SET_S
     }
   
   /// POWERs
-
-  sendGameAction({
-      type: "SET_BET_MISS",
-      betMissNumber: Number(value)
-    });
 if (action.type === "SET_BET_MISS") {
   state.powers.betMissNumber = action.betMissNumber;
   emitStateForAllPlayers(roomId, room, io);
