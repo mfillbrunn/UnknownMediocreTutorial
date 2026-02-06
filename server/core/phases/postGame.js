116b401f5f72161a0d968cfd9396ffe7cff8e17b
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-02-06 20:50:43 +0000

    Update postGame.js

--- server/core/phases/postGame.js
+++ server/core/phases/postGame.js
@@ -4,7 +4,7 @@ const { emitStateForAllPlayers } = require("../../utils/emitState");
 const { emitLobbyEvent } = require("../../utils/emitLobby");
 const { createInitialState } = require("../stateFactory");
 const resetRoundState = require("../../utils/resetRoundState");
-const {startGameTimer} = require("./normal");
+const { startGameTimer } = require("../timeouts/timeoutController");;
 
 function handleGameOverPhase(room, state, action, role, roomId, context) {
     const io = context.io;
