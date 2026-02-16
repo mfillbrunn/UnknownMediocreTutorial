9f81a3128f1a9ac0257b495a1debae6d09798b2b
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-02-15 22:29:24 -0500

    Update lobby.js

--- server/core/phases/lobby.js
+++ server/core/phases/lobby.js
@@ -161,6 +161,7 @@ if (action.type === "SET_DEV_MODE") {
           const players = Object.entries(room.players);
           const humanPlayers = players.filter(([_, p]) => !p.isAI);
           const aiPlayers = players.filter(([_, p]) => p.isAI);
+                console.log(action.mode);
          if (action.mode === "tutorial") {state.isTutorial = true;}               
          if (humanPlayers.length + aiPlayers.length < 2){return;}  
           state.ready[action.playerId] = true;               
