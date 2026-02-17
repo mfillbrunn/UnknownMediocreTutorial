6123f441c53c28cd9116b3a688f24bc8004ec62e
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-02-16 22:03:34 -0500

    Update client.js

--- public/client.js
+++ public/client.js
@@ -317,7 +317,6 @@ onStateUpdate(newState => {
   maybeStartRouletteFromState(state);
   if (state.phase === "simultaneous"){renderSetterRemainingBox(state, myRole, "");}
   if (state.phase === "normal"){renderSetterRemainingBox(state, myRole, state.secret);}
-  tutorialSteps(state, myRole);
   remainingCache.setterOld = null;
 });
 
