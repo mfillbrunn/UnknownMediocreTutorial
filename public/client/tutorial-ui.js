a411b82e35e758a207cf5f3ee16af5d933ef0798
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-02-22 22:06:02 -0500

    Update tutorial-ui.js

--- public/client/tutorial-ui.js
+++ public/client/tutorial-ui.js
@@ -187,6 +187,7 @@ window.notifyTutorialPowerUsed = notifyTutorialPowerUsed; // expose if needed
 function tutorialSteps(state, role) {
 const isGuesser = role === state.guesser;
 const isSetter  = role === state.setter;
+  if (state.phase === "Normal" && window.myRole != state.turn){return;}
   updateActionBadge();
   if (!state?.isTutorial) {
     lastTutorialRound = null;
