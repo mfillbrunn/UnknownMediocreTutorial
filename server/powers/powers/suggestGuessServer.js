e1a15f19a5c775559c5a9156b06abb51982bf106
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-03-18 22:09:14 -0400

    Update suggestGuessServer.js

--- server/powers/powers/suggestGuessServer.js
+++ server/powers/powers/suggestGuessServer.js
@@ -10,7 +10,7 @@ const WORDS = fs.readFileSync(path.join(__dirname, "../../wordlists/allowed_secr
   .split("\n");
 
 engine.registerPower("suggestGuess", {
-  apply(state, action, roomId, io, room) {
+  apply(state, action, roomId, io) {
 
     // Once per match
     if (state.powers.suggestGuessUsed) return false;
