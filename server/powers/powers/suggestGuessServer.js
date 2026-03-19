36fcf03aac6836a75bddccde1075d5753573dbd8
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-03-18 20:33:22 -0400

    Update suggestGuessServer.js

--- server/powers/powers/suggestGuessServer.js
+++ server/powers/powers/suggestGuessServer.js
@@ -30,9 +30,8 @@ engine.registerPower("suggestGuess", {
   console.log("suggestGuess sample:", feasible.slice(0,5));
   console.log("suggestGuess chosen:", suggestion);
     const socketId = state.players[action.playerId]?.socketId;
-    if (!action.ai) {
       io.to(socketId).emit("suggestWord", { word: suggestion });
-    }
+    
 
     io.to(roomId).emit("powerUsed", { type: "suggestGuess" });
   }
