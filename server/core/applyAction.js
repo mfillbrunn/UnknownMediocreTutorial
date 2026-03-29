cbf691986bf80beb519d377da200ef768b21f102
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-03-29 12:54:25 -0400

    Update applyAction.js

--- server/core/applyAction.js
+++ server/core/applyAction.js
@@ -3,19 +3,19 @@ const handleSimultaneousPhase = require("./phases/simultaneous");
 const { handleNormalPhase } = require("./phases/normal");
 const { handleGameOverPhase } = require("./phases/postGame");
 
-function applyAction(room, state, action, role, roomId, context) {
+function applyAction(room, state, action, roomId, context) {
   switch (state.phase) {
     case "lobby":
-      handleLobbyPhase(room, state, action, role, roomId, context);
+      handleLobbyPhase(room, state, action, roomId, context);
       break;
     case "simultaneous":
-      handleSimultaneousPhase(room, state, action, role, roomId, context);
+      handleSimultaneousPhase(room, state, action, roomId, context);
       break;
     case "normal":
-      handleNormalPhase(room, state, action, role, roomId, context);
+      handleNormalPhase(room, state, action, roomId, context);
       break;
     case "gameOver":
-      handleGameOverPhase(room, state, action, role, roomId, context);
+      handleGameOverPhase(room, state, action, roomId, context);
       break;
     default:
       console.warn("Unknown phase:", state.phase);
