babeb98680c40af403b292b96d98e744a8a46eca
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-03-29 13:13:15 -0400

    Update resetRoundState.js

--- server/utils/resetRoundState.js
+++ server/utils/resetRoundState.js
@@ -1,26 +1,40 @@
-const {createInitialPowers} = require("../core/stateFactory");
-const {resetRoundTimer} = require("./Timer");
+const { createInitialPowers } = require("../core/stateFactory");
+const { resetRoundTimer } = require("./Timer");
+
 function resetRoundState(room, state, roomId, context) {
   state.secret = "";
   state.pendingGuess = "";
   state.gameOver = false;
   state.guessCount = 0;
   state.history = [];
-  state.extraConstraints= [];
+  state.extraConstraints = [];
   state.turn = null;
   state.simultaneousGuessSubmitted = false;
   state.simultaneousSecretSubmitted = false;
   state.powerUsedThisTurn = false;
-  state.timeUsed.A = 0;
-  state.timeUsed.B = 0;
+  state.roundStartTime = null;
+  state.timeoutLoser = null;
+
   state.powersUsedThisRoundGuesser = [];
   state.powersUsedThisRoundSetter = [];
-   
- // clear transient power effects
-   state.powers = createInitialPowers(); 
-    if (state.timeControl.enabled) {
+
+  state.timeUsed ||= {};
+  state.roundTimeouts ||= {};
+  state.timeRemaining ||= {};
+
+  for (const userId of Object.keys(state.players || {})) {
+    state.timeUsed[userId] = 0;
+    state.roundTimeouts[userId] = 0;
+  }
+
+  // clear transient power effects
+  state.powers = createInitialPowers();
+
+  if (state.timeControl?.enabled) {
     resetRoundTimer(state);
     state.activeTimer = "both";
+  } else {
+    state.activeTimer = null;
   }
 }
 
