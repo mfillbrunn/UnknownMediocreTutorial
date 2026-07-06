5e3d292a16005fadf2b5d3934af59ef382b66149
mfillbrunn <145943764+mfillbrunn@users.noreply.github.com>
2026-07-05 22:07:25 -0400

    Update normalTransitions.js

--- server/core/transitions/normalTransitions.js
+++ server/core/transitions/normalTransitions.js
@@ -41,7 +41,7 @@ function transitionAfterGuess({ room, state, guess, roomId, context, io }) {
 
 function transitionAfterSecret({ room, state, secret, roomId, context, io }) {
   state.secret = secret;
-
+  state.simultaneousAllWrong = false;
   if (state.pendingGuess === secret) {
     pushWinEntry(state, secret);
     io.to(roomId).emit("secretFound");
